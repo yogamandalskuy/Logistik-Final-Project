ff1358799c820baa227fa269b3d59023c965dec9
yogamandalaw <yogamandalaw@gmail.com>
2024-05-31 12:40:50 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('LandingPage');
+});
+
+Route::get('/admin', function () {
+    return view('Admin');
+});
