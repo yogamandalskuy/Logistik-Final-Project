e3d47caaccd33171a016f05edd9220d8be9d482a
AmandA <78599553+yogamandalskuy@users.noreply.github.com>
2024-06-09 23:05:08 +0700

    Revert "Chsrio"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            StatusSeeder::class,
-            BorrowerSeeder::class
+            ItemSeeder::class,
             ]);
     }
 }
